d11d7e67668037febd2f188bc703e966318f664b
SpringHgui <springhgui@outlook.com>
2020-03-15 18:40:54 +0800

    111

--- src/store/auth.module.js
+++ src/store/auth.module.js
@@ -1,11 +1,116 @@
-const state = {}
-const getters = {}
-const actions = {}
-const mutations = {}
+import JwtService from "@/common/jwt.service";
+import ApiService from "@/common/api.service";
+import { SET_AUTH, PURGE_AUTH, SET_ERROR } from "./mutations.type";
+import {
+  LOGIN,
+  LOGOUT,
+  REGISTER,
+  CHECK_AUTH,
+  UPDATE_USER
+} from "./actions.type";
+
+const state = {
+  errors: null,
+  user: {},
+  isAuthenticated: !!JwtService.getToken()
+};
+
+const getters = {
+  currentUser(state) {
+    return state.user;
+  },
+  isAuthenticated(state) {
+    return state.isAuthenticated;
+  }
+}
+
+const mutations = {
+  [SET_ERROR](state, error) {
+    state.errors = error;
+  },
+  [SET_AUTH](state, user) {
+    state.isAuthenticated = true;
+    state.user = user;
+    state.errors = {};
+    JwtService.saveToken(state.user.token);
+  },
+  [PURGE_AUTH](state) {
+    state.isAuthenticated = false;
+    state.user = {};
+    state.errors = {};
+    JwtService.destroyToken();
+  }
+};
+
+const actions = {
+  [LOGIN](context, credentials) {
+    return new Promise(resolve => {
+      ApiService.post("users/login", credentials)
+        .then(({ data }) => {
+          if (data.success) {
+            context.commit(SET_AUTH, data.data);
+            resolve(data);
+          } else {
+            context.commit(SET_ERROR, data.errorMsg);
+          }
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors);
+        });
+    });
+  },
+  [LOGOUT](context) {
+    context.commit(PURGE_AUTH);
+  },
+  [REGISTER](context, credentials) {
+    return new Promise((resolve, reject) => {
+      ApiService.post("users", { user: credentials })
+        .then(({ data }) => {
+          context.commit(SET_AUTH, data.user);
+          resolve(data);
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors);
+          reject(response);
+        });
+    });
+  },
+  [CHECK_AUTH](context) {
+    if (JwtService.getToken()) {
+      ApiService.setHeader();
+      ApiService.get("user")
+        .then(({ data }) => {
+          context.commit(SET_AUTH, data.user);
+        })
+        .catch(({ response }) => {
+          context.commit(SET_ERROR, response.data.errors);
+        });
+    } else {
+      context.commit(PURGE_AUTH);
+    }
+  },
+  [UPDATE_USER](context, payload) {
+    const { email, username, password, image, bio } = payload;
+    const user = {
+      email,
+      username,
+      bio,
+      image
+    };
+    if (password) {
+      user.password = password;
+    }
+
+    return ApiService.put("user", user).then(({ data }) => {
+      context.commit(SET_AUTH, data.user);
+      return data;
+    });
+  }
+};
 
 export default {
-    state,
-    getters,
-    actions,
-    mutations,
+  state,
+  getters,
+  actions,
+  mutations,
 };
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -30,8 +30,10 @@
 </template>
 
 <script>
+import { mapState } from "vuex";
 import checks from "@/utils/checks.js";
-import signService from "@/common/user.service.js";
+// import signService from "@/common/user.service.js";
+import { LOGIN } from "@/store/actions.type";
 
 export default {
   data() {
@@ -66,34 +68,20 @@ export default {
       let that = this;
       this.$refs[formName].validate(valid => {
         if (valid) {
-          signService
-            .login(this.ruleForm)
-            .then(res => {
-              if (res.data.success) {
-                // {"success":true,"errorCode":0,"errorMsg":"登录成功","data":{"token":"90b2ab24-831f-4f22-a689-b71bc070974d","expire_time":"2020-03-15T00:10:01.1959311+08:00"}}
-                // 登录成功
-                console.log(res.data);
-              } else {
-                that.$message({
-                  showClose: true,
-                  message: res.data.errorMsg,
-                  type: "error"
-                });
-              }
-            })
-            .catch(error => {
-              that.$message({
-                showClose: true,
-                message: error,
-                type: "error"
-              });
-            });
+          that.$store
+            .dispatch(LOGIN, this.ruleForm)
+            .then(() => this.$router.push({ name: "home" }));
         } else {
           console.log("error submit!!");
           return false;
         }
       });
     }
+  },
+  computed: {
+    ...mapState({
+      errors: state => state.auth.errors
+    })
   }
 };
 </script>
