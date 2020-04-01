2ce805e1f1847cc068d715bbec94295151600cda
SpringHgui <springhgui@outlook.com>
2020-04-02 00:05:37 +0800

    添加下载地址

--- src/views/Console/Download.vue
+++ src/views/Console/Download.vue
@@ -9,13 +9,29 @@
 .link {
   color: red;
 }
+
+.down-box {
+  padding-top: 50px;
+}
+.down-box a {
+  padding-right: 50px;
+}
 </style>
 
 <template>
-    <div class="container-xl">
-        <div class="title">欢迎使用FastTunnel</div>
-        <div></div>
+  <div class="container-xl">
+    <div class="title">请根据您的PC版本下载对应的客户端</div>
+    <div>
+      <a href="https://github.com/SpringHgui/FastTunnel/releases" target="_blank">去Gitbhu下载最新客户端</a>
     </div>
+    
+    <div class="down-box">
+      <a href="/SuiDao.Client.win-x64.zip" target="_blank">win-x64</a>
+      <a href="/SuiDao.Client.osx-x64.zip" target="_blank">osx-x64</a>
+      <a href="/SuiDao.Client.linux-x64.zip" target="_blank">linux-x64</a>
+    </div>
+    <div></div>
+  </div>
 </template>
 
 <script>
