1255d3ce11d86622e7a19a83c842ad6a3412022e
SpringHgui <740360381@qq.com>
2020-10-09 11:40:44 +0800

    添加字体

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -33,38 +33,69 @@
   top: 8px;
   left: -11px;
 }
+#logo {
+  font-family: "Dosis";
+  font-size: 1.5rem;
+}
 </style>
 
 <template>
   <div class="container-xl">
     <div class="menu">
       <div class="menu-item">
-        <a href="/#/" class="nav-link router-link-exact-active active">
+        <a
+          href="/#/"
+          class="nav-link router-link-exact-active active"
+          id="logo"
+        >
           <!-- <el-image class="logo" src="/logo.png" fit="fit"></el-image> -->
           SuiDao
         </a>
       </div>
-      <div class="menu-item" style="float: right;">
+      <div
+        class="menu-item"
+        style="float: right;"
+      >
         <div v-if="!isAuthenticated">
-          <el-button size="small" type="text" @click="login">登录</el-button>
-          <el-button size="small" @click="signup">注册</el-button>
+          <el-button
+            size="small"
+            type="text"
+            @click="login"
+          >登录</el-button>
+          <el-button
+            size="small"
+            @click="signup"
+          >注册</el-button>
         </div>
         <div v-if="isAuthenticated">
-          <a href="/console.html#/" class="nav-link router-link-exact-active active">
-            <div class="menu-item" style="padding-right: 45px;">控制台</div>
+          <a
+            href="/console.html#/"
+            class="nav-link router-link-exact-active active"
+          >
+            <div
+              class="menu-item"
+              style="padding-right: 45px;"
+            >控制台</div>
           </a>
           <el-dropdown :hide-on-click="false">
             <span class="el-dropdown-link">
               {{ currentUser && currentUser.profile ? currentUser.profile.name:""}}
-              <i
-                class="el-icon-arrow-down el-icon--right"
-              ></i>
+              <i class="el-icon-arrow-down el-icon--right"></i>
             </span>
-            <el-dropdown-menu :hide-on-click="true" slot="dropdown">
-              <router-link :to="{ name: 'profile'}" class="el-link el-link--default">
+            <el-dropdown-menu
+              :hide-on-click="true"
+              slot="dropdown"
+            >
+              <router-link
+                :to="{ name: 'profile'}"
+                class="el-link el-link--default"
+              >
                 <el-dropdown-item>个人中心</el-dropdown-item>
               </router-link>
-              <el-dropdown-item @click.native="logOut" divided>退出登录</el-dropdown-item>
+              <el-dropdown-item
+                @click.native="logOut"
+                divided
+              >退出登录</el-dropdown-item>
             </el-dropdown-menu>
           </el-dropdown>
         </div>
