b58f3574d2dd822f586e853f8d650679e52fd7e5
SpringHgui <740360381@qq.com>
2020-10-10 17:53:56 +0800

    丰富一下主页

--- src/module/index/views/Home.vue
+++ src/module/index/views/Home.vue
@@ -4,19 +4,173 @@
   font-size: 34px;
   text-align: center;
 }
-.download{
-  padding-top: 150px;
-  font-size: 22px;
+.download {
+  padding-top: 100px;
+  padding-bottom: 50px;
+  font-size: 3rem;
   text-align: center;
+}
+.notice-title {
+  font-size: 1.5rem;
+  text-align: center;
+  padding: 15px;
+}
+.notice-title-sub {
+  font-size: 1rem;
+  text-align: center;
+  padding: 5px 0 15px 0;
+}
+.notice-box .notice-box-title {
+  text-align: center;
+  font-size: 1.2rem;
+  color: #03a8fa;
+}
+.notice-box {
+  min-height: 150px;
 }
 </style>
 
 <template>
   <div>
-    <div class="welcome">Welcome to SuiDao！</div>
-    <div class="welcome">基于开源<el-link :underline="false" href="https://github.com/SpringHgui/FastTunnel" target="_blank">FastTunnel</el-link>实现的内网穿透服务</div>
+    <div class="container-xl">
+      <div class="welcome">基于开源<el-link
+          :underline="false"
+          href="https://github.com/SpringHgui/FastTunnel"
+          target="_blank"
+        >FastTunnel</el-link>实现的内网穿透服务</div>
+    </div>
+    <div class="container-xl">
+      <div class="download">
+        <el-link
+          :underline="false"
+          href="https://github.com/SpringHgui/FastTunnel.SuiDao/releases"
+          target="_blank"
+        >下载客户端</el-link>
+      </div>
+    </div>
+    <!-- 场景 -->
+    <div class="container-xl">
+      <div class="notice-title">使用场景</div>
+      <div class="notice-title-sub">多种应用场景，你能想到的都能做到！</div>
+      <div class="notice-title-sub">更多惊喜，等你发现！</div>
+      <el-row :gutter="10">
+        <el-col
+          :xs="24"
+          :sm="12"
+          :md="6"
+          :lg="6"
+        >
+          <el-card class="box-card">
+            <div class="notice-box">
+              <div class="notice-box-title">家中建站</div>
+              <p class="notice-box-body">我是站长，无需公网IP，不用机房托管，虚拟主机的价格，独立服务器的配置，建网站，私有云。</p>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col
+          :xs="24"
+          :sm="12"
+          :md="6"
+          :lg="6"
+        >
+          <el-card class="box-card">
 
-    <div class="download"><el-link :underline="false" href="https://github.com/SpringHgui/FastTunnel.SuiDao/releases" target="_blank">下载客户端</el-link></div>
+            <div class="notice-box">
+              <div class="notice-box-title">远程桌面</div>
+              <p class="notice-box-body">我常出差：链接系统远程桌面，在外模拟家中电脑，手机也能远程桌面，提取资料简单快捷。</p>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col
+          :xs="24"
+          :sm="12"
+          :md="6"
+          :lg="6"
+        >
+          <el-card class="box-card">
+            <div class="notice-box">
+              <div class="notice-box-title">ERP互联互通</div>
+              <p class="notice-box-body">我是账务，支持OA办公/财务软件/连锁店管理，把分支办公室连起来，还支持远程操作打印机。</p>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col
+          :xs="24"
+          :sm="12"
+          :md="6"
+          :lg="6"
+        >
+          <el-card class="box-card">
+
+            <div class="notice-box">
+              <div class="notice-box-title">SVN代码库</div>
+              <p class="notice-box-body">我是程序员，抱上笔记本，来一场说走就走的旅行，知你如知己，正如写到这，想到了你此刻的目光。</p>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
+    <!-- 特色 -->
+    <div class="container-xl">
+      <div class="notice-title">产品特色</div>
+      <el-row :gutter="10">
+        <el-col
+          :xs="24"
+          :sm="12"
+          :md="6"
+          :lg="6"
+        >
+          <el-card class="box-card">
+
+            <div class="notice-box">
+              <div class="notice-box-title">跨平台客户端支持</div>
+              <p class="notice-box-body">支持多个平台的客户端, window, linux, mac等.</p>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col
+          :xs="24"
+          :sm="12"
+          :md="6"
+          :lg="6"
+        >
+          <el-card class="box-card">
+
+            <div class="notice-box">
+              <div class="notice-box-title">多种安全级别控制</div>
+              <p class="notice-box-body">可以根据需要设置通过域名白名单，账号验证访问，数据加密等。</p>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col
+          :xs="24"
+          :sm="12"
+          :md="6"
+          :lg="6"
+        >
+          <el-card class="box-card">
+
+            <div class="notice-box">
+              <div class="notice-box-title">无需设置路由端口映射</div>
+              <p class="notice-box-body">第三代内网穿透工具，无需设置路由端口映射，无需公网IP，很多用户都是动态IP，没有关系一样可以实现内网穿透。</p>
+            </div>
+          </el-card>
+        </el-col>
+        <el-col
+          :xs="24"
+          :sm="12"
+          :md="6"
+          :lg="6"
+        >
+          <el-card class="box-card">
+            <div class="notice-box">
+              <div class="notice-box-title">永久免费</div>
+              <p class="notice-box-body">所有注册用户均永久免费提供穿透服务</p>
+            </div>
+          </el-card>
+        </el-col>
+      </el-row>
+    </div>
   </div>
 </template>
 
